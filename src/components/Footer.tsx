8ba740af951ee65435afc5d94845602e170a667b
wattanx <wattan.dev@gmail.com>
2022-01-25 23:14:37 +0900

    fix: footerをいい感じに修正

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,37 @@
-import { Flex, FlexProps } from '@chakra-ui/react'
+import {
+  Center,
+  chakra,
+  Text,
+  FlexProps,
+  Stack,
+  useColorMode,
+} from '@chakra-ui/react';
 
-export const Footer = (props: FlexProps) => (
-  <Flex as="footer" py="8rem" {...props} />
-)
+export const Footer = (props: FlexProps) => {
+  const { colorMode } = useColorMode();
+  return (
+    <chakra.footer
+      backgroundColor={colorMode === 'dark' ? '#000' : '#fff'}
+      padding="7"
+    >
+      <Center>
+        <Stack>
+          <Text fontSize="sm" paddingBottom="2" textAlign="center">
+            © {new Date().getFullYear()} wattanx
+          </Text>
+          <Text fontSize="sm">
+            This site uses Google Analytics.{` `}
+            <chakra.a
+              href="https://policies.google.com/technologies/partner-sites?hl=ja"
+              target="_blank"
+              rel="external noopener"
+              textDecoration="underline"
+            >
+              See more details
+            </chakra.a>
+          </Text>
+        </Stack>
+      </Center>
+    </chakra.footer>
+  );
+};
