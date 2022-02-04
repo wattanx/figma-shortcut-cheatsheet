ea25d563c6c376f85b99bd252b65c17301b56294
wattanx <wattan.dev@gmail.com>
2022-02-04 13:17:09 +0900

    tagmanager入れてみる

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -23,6 +23,20 @@ export default class Document extends NextDocument {
             `,
             }}
           />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `(function (w, d, s, l, i) {
+              w[l] = w[l] || [];
+              w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' });
+              var f = d.getElementsByTagName(s)[0],
+                j = d.createElement(s),
+                dl = l != 'dataLayer' ? '&l=' + l : '';
+              j.async = true;
+              j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+              f.parentNode.insertBefore(j, f);
+            })(window, document, 'script', 'dataLayer', 'GTM-M8TR4RT');`,
+            }}
+          />
           <link rel="icon" type="image/svg+xml" href="/favicon.svg"></link>
           <link rel="apple-touch-icon" href="/apple-touch-icon.png"></link>
         </Head>
