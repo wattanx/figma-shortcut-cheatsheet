23273b5d22e1bf0616d5ea61ad0e3040e2371ea0
wattanx <wattan.dev@gmail.com>
2022-06-24 12:29:24 +0900

    fix: UAの接続済みサイトタグができるか実験

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -17,9 +17,6 @@ export default class Document extends NextDocument {
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
-              gtag('config', '${GA_TRACKING_ID}', {
-                page_path: window.location.pathname,
-              });
             `,
             }}
           />
