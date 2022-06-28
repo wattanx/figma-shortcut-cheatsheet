281d47289e28bd174c2cbc6dfc27c8a301942e59
wattanx <wattan.dev@gmail.com>
2022-06-28 10:17:20 +0900

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -17,7 +17,9 @@ export default class Document extends NextDocument {
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
-             
+              gtag('config', '${GA_TRACKING_ID}', {
+                page_path: window.location.pathname,
+              });
             `,
             }}
           />
